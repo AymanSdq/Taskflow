afdfecdef3039fbb5ac4e7b581cef4b4a377a9ee
AymanSdq <sedqiayman02@gmail.com>
2025-03-26 00:24:28 +0000

    Dark mode handeling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-
+import { Link } from "react-router"
+import { ModeToggle } from "./components/mode-toggle"
 
 function App() {
 
 
   return (
       <section>
-        
+        <button><Link to="/register">Register</Link></button>
+        Or
+        <button><Link to="/Login">Login</Link></button>
       </section>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,25 @@ import Register from './pages/Register.tsx'
 import Login from './pages/Login.tsx'
 import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ThemeProvider } from "@/components/theme-provider"
 
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
+    {/* DEvo tool */}
     <ReactQueryDevtools initialIsOpen={false} />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    {/* Theme handlers */}
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        {/* Routes */}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
   </QueryClientProvider>
 )
