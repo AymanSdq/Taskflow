40c2625b0b0cbd9b7b0e3edec7121e9d941a811b
AymanSdq <sedqiayman02@gmail.com>
2025-03-31 20:05:31 +0000

    Error fetching data must be fixed

--- src/pages/Profileedit.tsx
+++ src/pages/Profileedit.tsx
@@ -1,12 +1,32 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useUserData } from "@/services/query"
+
+
+
 
 
 const Profileedit = () => {
+
+
+    const { data : userData , isLoading : userDataLoading, isError : userDataError, isSuccess : userDataSuccess } = useUserData();
+
+
+
     return (
         <div className="flex flex-1 flex-col gap-4 p-4">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        {userDataSuccess &&  <>
+
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    {/* Fullname */}
+                    <span className="truncate font-medium">{userData.fullname}</span>
+                    {/* Email */}
+                    <span className="truncate text-xs">{userData.email}</span>
+                  </div>
+                </>
+              }
             <div className="col-span-1">
                 <form action="" className="py-4">
                     <h1 className="text-xl font-bold">User settings : </h1>
@@ -18,7 +38,7 @@ const Profileedit = () => {
                     {/* User */}
                     <div className="grid w-full my-4 max-w-sm items-center gap-3">
                         <Label htmlFor="fullname">Fullname</Label>
-                        <Input type="text" id="fullname" placeholder="Fullname" />
+                        <Input value={userData.data.fullname} type="text" id="fullname" placeholder="Fullname" />
                     </div>
                     <Button className="my-4">Save</Button>
                 </form>
