f76a8805793823e382274a90e8e257583a401b8f
AymanSdq <sedqiayman02@gmail.com>
2025-03-31 23:57:22 +0000

    Fixing form and getting data issue

--- src/pages/Profileedit.tsx
+++ src/pages/Profileedit.tsx
@@ -6,27 +6,38 @@ import { useUserData } from "@/services/query"
 
 
 
-
 const Profileedit = () => {
 
 
     const { data : userData , isLoading : userDataLoading, isError : userDataError, isSuccess : userDataSuccess } = useUserData();
 
-
+    console.log(userData)
 
     return (
         <div className="flex flex-1 flex-col gap-4 p-4">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        {userDataSuccess &&  <>
+            <div className="col-span-1 w-full py-4 px-4 flex flex-col items-center border rounded-md ">
+                <div className="w-20 mb-4">
+                    {userDataSuccess && <img src={userData.data.avatarurl} alt={userData.data.fullname} />}
+                    {userDataError || userDataLoading && <img src="" alt="Avatar" />}
+                </div>
+                <div className="w-full">
+                    <div className="grid w-full my-4 max-w-sm items-center gap-3">
+                        <Label htmlFor="fullname">Full name</Label>
+                        <Input className="text-white" value={userDataSuccess && userData.data.fullname}  type="text" id="fullname" placeholder="Fullname" disabled/>
+                    </div>
+                    <div className="grid w-full my-4 max-w-sm items-center gap-3">
+                        <Label htmlFor="email">Email</Label>
+                        <Input value={userDataSuccess && userData.data.email}  type="text" id="email" placeholder="Email" disabled/>
+                    </div>
+                    <div className="grid w-full my-4 max-w-sm items-center gap-3">
+                        <Label htmlFor="email">Date of creation</Label>
+                        <Input value={userDataSuccess && userData.data.updated_At}  type="date" id="email" placeholder="Email" disabled/>
+                    </div>
+                </div>
+            </div>
 
-                  <div className="grid flex-1 text-left text-sm leading-tight">
-                    {/* Fullname */}
-                    <span className="truncate font-medium">{userData.fullname}</span>
-                    {/* Email */}
-                    <span className="truncate text-xs">{userData.email}</span>
-                  </div>
-                </>
-              }
+            {/* Formulater */}
             <div className="col-span-1">
                 <form action="" className="py-4">
                     <h1 className="text-xl font-bold">User settings : </h1>
@@ -38,10 +49,12 @@ const Profileedit = () => {
                     {/* User */}
                     <div className="grid w-full my-4 max-w-sm items-center gap-3">
                         <Label htmlFor="fullname">Fullname</Label>
-                        <Input value={userData.data.fullname} type="text" id="fullname" placeholder="Fullname" />
+                        <Input  type="text" id="fullname" placeholder="Fullname" />
                     </div>
                     <Button className="my-4">Save</Button>
                 </form>
+            </div>
+            <div>
                 <form action="" className="py-4">
                     <h1 className="text-xl font-bold">Password settings : </h1>
                     {/* User */}
